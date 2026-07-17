b7e3c14d5877cc9cab41825070ed5e85ac086c5d
Vishva Velu <82202090+vishva2203@users.noreply.github.com>
2026-07-17 12:01:41 +0530

    Add files via upload

---
+++ AURA_DELAY.c
@@ -0,0 +1,23 @@
+#include "header.h"
+
+void delay_sec(int sec)
+{
+    T0PR  = 15000000-1;
+    T0TCR = 0x01;
+
+    while(T0TC < sec);
+
+    T0TCR = 0x03;
+    T0TCR = 0x00;
+}
+
+void delay_ms(int ms)
+{
+    T0PR  = 15000-1;
+    T0TCR = 0x01;
+
+    while(T0TC < ms);
+
+    T0TCR = 0x03;
+    T0TCR = 0x00;
+}
